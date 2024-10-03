c907c30a7ba36d062c9d457880ce3f349343cb33
happy-birthday-100 <himap1323@gmail.com>
2024-10-03 16:28:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,27 +21,26 @@ document.getElementById('f').addEventListener('submit', function(event) {
     const password = document.getElementById('password').value;
     console.log(username, password);
     // Simple validation (for demonstration purposes)
-    if (username === '1' && password === '1') {
+    if (username === 'Takshita' && password === 'aryan') {
         document.getElementById('f').style.display = 'none';
         const message = document.getElementById('birthdayMessage');
         message.style.display = 'block';
-        message.style.fontSize = '60px'; // Make the message larger
+        message.style.fontSize = '40px'; // Make the message larger
         const message2 = document.getElementById('him');
         message2.style.display = 'block';
-        message2.style.fontSize = '15px';
+        message2.style.fontSize = '10px';
         const messageq = document.getElementById('ag');
         messageq.style.display = 'block';
-        messageq.style.fontSize = '15px';
+        messageq.style.fontSize = '10px';
         const message3 = document.getElementById('go');
         message3.style.display = 'block';
-        message3.style.fontSize = '15px';
+        message3.style.fontSize = '10px';
         const message4 = document.getElementById('sh');
         message4.style.display = 'block';
-        message4.style.fontSize = '15px';
+        message4.style.fontSize = '10px';
         const message5 = document.getElementById('ja');
         message5.style.display = 'block';
-        message5.style.fontSize = '15px';
-        
+        message5.style.fontSize = '10px';
         // Change the background image
         document.body.style.backgroundImage = "url('img.jpg')";
         document.body.style.backgroundSize = 'fit';
